ef51c27949c99f4d77c5cfed97134fe07deaf8c9
aspri951 <amy.springer@aggiemail.usu.edu>
2016-10-24 10:49:30 -0600

    better loop, bad brackets

--- Lesson_2_ASpringer.R
+++ Lesson_2_ASpringer.R
@@ -1,11 +1,18 @@
 #Session 2 Exercises
 
-#1: Write loop printing 20-->10
+#1: Write loop that prints 20-->10
 
 value <- 21
 for (i in 1:11){
   print(value <- value-1)
 }
+
+for(i in 20:10){
+  print(i)
+}
+
+# i CANNOT be in square brackets or an error is returned
+
 
 #2 Write loop that prints 20-->10, evens only
 
