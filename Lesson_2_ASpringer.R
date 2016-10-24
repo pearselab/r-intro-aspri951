af310ebc01774a55d46191544b62adee4e6a6d23
aspri951 <amy.springer@aggiemail.usu.edu>
2016-10-24 11:25:07 -0600

    another wrong way to do things

--- Lesson_2_ASpringer.R
+++ Lesson_2_ASpringer.R
@@ -7,6 +7,8 @@ for (i in 1:11){
   print(value <- value-1)
 }
 
+#better way:
+
 for(i in 20:10){
   print(i)
 }
@@ -14,6 +16,10 @@ for(i in 20:10){
 # i CANNOT be in square brackets or an error is returned
 
 
-#2 Write loop that prints 20-->10, evens only
+#2: Write loop that prints 20-->10, evens only
+
+sort(seq(from = 10, to = 20, by = 2), decreasing = TRUE)
+
+# not a loop, but gives the right answer...
 
 
