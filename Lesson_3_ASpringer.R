1cc6568642cc9440efc24d5020ba734e164e17c8
aspri951 <amy.springer@aggiemail.usu.edu>
2016-10-26 11:16:00 -0600

    more questions

--- Lesson_3_ASpringer.R
+++ Lesson_3_ASpringer.R
@@ -1,3 +1,13 @@
+#1) Implement cat class, slot = race, methods = pring
+
 class(cat) <- "race"
 
 #How does R define the slots for an class? It appears that you have to define an object first and then R uses that instance as the example/model that defines the class? In other words, you cannot define a class without first giving an instance? 
+#Apparently you can't just define a class. Attempt to use an instance to define a class:
+
+Milo <- list(race = "siamese")
+class(Milo) <- "cat"
+
+#Again, this is NOT intuitive to me. Just looking at the code, it appears that I have just taken a specific case and generalized it to the entire class (induction). In this case, it looks like I just said "because Milo is Siamese, and Milo is a cat, all cats are siamese." 
+
+
