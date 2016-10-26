843da1bb493158c645cfaabcd889f1ac33b2638e
aspri951 <amy.springer@aggiemail.usu.edu>
2016-10-26 11:20:09 -0600

    still on question 1

--- Lesson_3_ASpringer.R
+++ Lesson_3_ASpringer.R
@@ -10,4 +10,7 @@ class(Milo) <- "cat"
 
 #Again, this is NOT intuitive to me. Just looking at the code, it appears that I have just taken a specific case and generalized it to the entire class (induction). In this case, it looks like I just said "because Milo is Siamese, and Milo is a cat, all cats are siamese." 
 
+Darwin <- list(race = "Russian blue")
+
+#I have given a second object the same property (slot) as my first instance of cat. However, R does NOT appear to assign "Darwin" to class "cat." 
 
